c0f2e74bf4f49ba11ac4fe4095ccd796483d54fd
genesis-g101 <gyreplicant@gmail.com>
2022-10-13 14:11:09 -0300

    criando a estrutura base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import './App.css';
+import poweredImage from './assets/icons/powered.png';
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <header className="max-w-screen-lg my-10 mx-auto">
+        <div>
+          <img className="inline-block" src={poweredImage} alt="logo" width={150} />
+        </div>
+      </header>
+      <div className="flex max-w-screen-lg m-auto gap-10">
+        <div className="grow bg-fuchsia-500">left</div>
+        <div className="grow bg-cyan-300">right</div>
+      </div>
     </div>
   );
 };
