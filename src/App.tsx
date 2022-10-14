6e861f0f4a8b8a02e550763803e8e132bf8a64ed
genesis-g101 <gyreplicant@gmail.com>
2022-10-14 12:45:40 -0300

    criando o leftside parte 2

--- src/App.tsx
+++ src/App.tsx
@@ -21,33 +21,39 @@ const App = () => {
   };
   return (
     <div className="wrap">
-      <header className="max-w-4xl my-10 mx-auto">
-        <div>
+      <header>
+        <div className="max-w-4xl my-10 mx-auto">
           <img className="inline-block" src={poweredImage} alt="logo" width={150} />
         </div>
       </header>
       <div className="flex max-w-4xl m-auto gap-10">
         <div className="grow">
-          <h1 className="text-4xl">Calcule o seu IMC</h1>
-          <p className="text-base mb-10">
+          <h1 className="m-0 mb-5 text-4xl text-indigo-900">Calcule o seu IMC</h1>
+          <p className="text-base text-gray-500 mb-10">
             IMC é a sigla para indice de massa corporea, <br />
             paramêtro adotado pela organização mundial de saude
             <br /> e usada para calcular se uma pessoa está no peso ideal.
           </p>
           {/* bg-fuchsia-500 */}
           <input
+            className="w-full border-0 border-b-2 border-solid border-neutral-300 py-2.5 px-0.5 mb-5 text-sm outline-none"
             type="number"
             placeholder="Digite seu peso, Ex: 65.3 (em kg)"
             value={weightField > 0 ? weightField : ''}
             onChange={weightChangeHandler}
           />
           <input
+            className="w-full border-0 border-b-2 border-solid border-neutral-300 py-2.5 px-0.5 mb-5 text-sm outline-none"
             type="number"
             placeholder="Digite sua altura, Ex: 1.7 (em metros)"
             value={heightField > 0 ? heightField : ''}
             onChange={heightChangeHandler}
           />
-          <button type="submit" onClick={calculateHandler}>
+          <button
+            className="bg-cyan-600 text-white text-sm border-0 rounded-lg py-3.5 w-full cursor-pointer mt-10 transition ease-in-out  hover:opacity-80"
+            type="submit"
+            onClick={calculateHandler}
+          >
             Calcular
           </button>
         </div>
