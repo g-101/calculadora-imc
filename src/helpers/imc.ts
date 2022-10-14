5956a6ce9d7001ecb3de2153ad862e6949e9e424
genesis-g101 <gyreplicant@gmail.com>
2022-10-14 13:10:46 -0300

    criando os helpers e calculateIMC

---
+++ src/helpers/imc.ts
@@ -0,0 +1,24 @@
+export type Level = {
+  title: string;
+  color: string;
+  icon: 'up' | 'down';
+  imc: number[];
+  yourImc?: number;
+};
+
+export const levels: Level[] = [
+  { title: 'Abaixo do Peso', color: '#96A3AB', icon: 'down', imc: [0, 18.5] },
+  { title: 'Peso Normal', color: '#0EAD69', icon: 'up', imc: [18.6, 24.9] },
+  { title: 'SobrePeso', color: '#E2B039', icon: 'down', imc: [25, 30] },
+  { title: 'Obesidade', color: '#C3423F', icon: 'down', imc: [30.1, 99] },
+];
+export const calculateImc = (height: number, weight: number) => {
+  const imc = weight / (height * height);
+  for (let index in levels) {
+    if (imc >= levels[index].imc[0] && imc < levels[index].imc[1]) {
+      levels[index].yourImc = imc;
+      return levels[index];
+    }
+  }
+  return null;
+};
